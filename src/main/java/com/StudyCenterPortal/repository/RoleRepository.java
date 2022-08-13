f26d4af960800c81ba640082da82c655ed23a3a7
tolie <tolie@infini.local>
2022-08-13 14:29:39 +0700

    create students ui

--- src/main/java/com/StudyCenterPortal/repository/RoleRepository.java
+++ src/main/java/com/StudyCenterPortal/repository/RoleRepository.java
@@ -3,7 +3,9 @@ package com.StudyCenterPortal.repository;
 
 import com.StudyCenterPortal.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Role findByName(String name);
---
+++ src/main/java/com/StudyCenterPortal/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.StudyCenterPortal.repository;
+
+import com.StudyCenterPortal.dto.UserDto;
+import com.StudyCenterPortal.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<User, Long> {
+
+    User createStudents(UserDto userDto);
+}
